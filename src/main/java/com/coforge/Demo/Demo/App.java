08c3e3d9b6e37ac353189c7844c5d5a3b05f296a
anubha921 <59173421+anubha921@users.noreply.github.com>
2021-02-23 11:59:05 +0530

    Update App.java
    I apologise Abhishek

--- src/main/java/com/coforge/Demo/Demo/App.java
+++ src/main/java/com/coforge/Demo/Demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Abhishek pagal" );
+        System.out.println( "Hello Abhishek, now u are good boy" );
     }
 }
