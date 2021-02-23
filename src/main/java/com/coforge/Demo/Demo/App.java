3d17eca82f9452b26c3cdf0b060b00002edafd86
Anubha Garg <Anubha Garg@DESKTOP-7FK2S3N>
2021-02-23 11:52:57 +0530

    pagal abhishek

---
+++ src/main/java/com/coforge/Demo/Demo/App.java
@@ -0,0 +1,13 @@
+package com.coforge.Demo.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Abhishek pagal" );
+    }
+}
